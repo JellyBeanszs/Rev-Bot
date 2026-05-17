8dc13adf98cc73ebcf1ddf5c16975820571b2125
JellyBeanszs <primrose.102623@gmail.com>
2026-05-17 12:18:53 +0800

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,9 @@ def home():
     return "Project Rev RP Bot is Alive and Running 24/7!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # FIXED: Automatically uses Render's assigned port (defaulting to 10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
