be3e0d57fcc4ab0effefc59eb41370cd32bd1505
JellyBeanszs <primrose.102623@gmail.com>
2026-05-17 10:10:13 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -230,7 +230,7 @@ async def setup_whitelist(ctx):
             "**📋 SERVER REQUIREMENTS:**\n"
             "🔹 You must possess a legal copy of Grand Theft Auto V.\n"
             "🔹 You must have a functional headset / microphone setup.\n"
-            "🔹 Detailed character backgrounds provide higher approval rates.\n\n"
+            "🔹 Detailed character backgrounds provide higher approval rates.\n\n\n"
             "⚠️ **IMPORTANT NAME RULE:**\n"
             "The name you fill out in the **👤 Name** field on this application form "
             "**must match your FiveM client profile name exactly** when connecting to our "
@@ -238,6 +238,7 @@ async def setup_whitelist(ctx):
         ), 
         color=discord.Color.from_rgb(46, 204, 113)
     )
+
 
     embed.set_footer(text="Click the green button below to open up your registration pop-up.")
     await ctx.send(embed=embed, view=SetupView())
