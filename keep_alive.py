5d12bc76eef6cc0f32cca988aa3c6e4384eac1bd
JellyBeanszs <primrose.102623@gmail.com>
2026-05-17 12:27:37 +0800

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Project Rev RP Bot is Alive and Running 24/7!"
 
 def run():
-    # FIXED: Automatically uses Render's assigned port (defaulting to 10000)
+    # Forces the bot to use Render's dynamic port variable
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
